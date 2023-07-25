70b84bbdb943fe82c82dfb813a5ac101765a290c
Umpha <Tshivhulaumpha11@gmail.com>
2023-07-25 14:01:14 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/***
+ * *_strcpy - copies the string
+ * @dest: string of type char
+ * @src: string of type char
+ * Description: Copy the string pointed to by 'src'
+ * to the buffer pointed by 'dest'
+ * Return: Pointer to 'dest'
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int n = -1;
+
+do {
+	n++;
+	dest[n] = src[n];
+} while (src[n] != '\0');
+return (dest);
+	}
